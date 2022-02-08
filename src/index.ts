c4fdc4795ef3c765ea8c2a66c0abe6dd6b28daa1
Lukasz Ostrowski <lukasz@monolog.tech>
2022-02-08 16:04:15 +0100

    Add TS

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import { data } from "./data";
+import { renderRadar } from "./radar";
+
+renderRadar({
+  svg_id: "radar",
+  width: 1450,
+  height: 1000,
+  colors: {
+    background: "#F0EEEA",
+    grid: "#CECFD2",
+    inactive: "#ddd",
+  },
+  quadrants: [
+    { name: "Languages & Frameworks" },
+    { name: "TODO" },
+    { name: "TODO" },
+    { name: "TODO" },
+  ],
+  rings: [
+    { name: "ADOPT", color: "#EB4646", textColor: "#DBD1C7" },
+    { name: "TRIAL", color: "#CAE0DC", textColor: "#001932" },
+    { name: "ASSESS", color: "#DBD1C7", textColor: "#001932" },
+    { name: "HOLD", color: "#001932", textColor: "#DBD1C7" },
+  ],
+  print_layout: true,
+  entries: data,
+});
