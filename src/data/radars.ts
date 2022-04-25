7e50222c3373328c81a5ac305da6209ad0cc7e2b
Lukasz Ostrowski <lukasz@monolog.tech>
2022-04-25 20:25:03 +0200

    Revert "Feature/cms integration"

---
+++ src/data/radars.ts
@@ -0,0 +1,12 @@
+export const radars = [
+  {
+    title: "NoA Ignite PL - Tech Radar Feb 2022",
+    url: "/",
+    link: "NoA Tech Radar",
+  },
+  {
+    title: "NoA Ignite PL - Martech Radar Feb 2022",
+    url: "/martech",
+    link: "NoA Martech Radar",
+  },
+];
