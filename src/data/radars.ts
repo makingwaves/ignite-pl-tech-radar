a7a7d5194d0490d6d26f8758288106e7a9c75fc9
Sebastian <sm.boloz96@gmail.com>
2022-04-26 16:11:32 +0200

    radar title changed

--- src/data/radars.ts
+++ src/data/radars.ts
@@ -1,11 +1,11 @@
 export const radars = [
   {
-    title: "NoA Ignite PL - Tech Radar Feb 2022",
+    title: "NoA Ignite PL - Tech Radar Apr 2022",
     url: "/",
     link: "NoA Tech Radar",
   },
   {
-    title: "NoA Ignite PL - Martech Radar Feb 2022",
+    title: "NoA Ignite PL - Martech Radar Apr 2022",
     url: "/martech",
     link: "NoA Martech Radar",
   },
