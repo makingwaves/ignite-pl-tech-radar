c3b138404636cc6bcb81033ad2a43b4d3a516024
Sebastian <sm.boloz96@gmail.com>
2022-04-29 12:11:38 +0200

    Updates

--- _next/static/RQ6ZgZZ03GwmX3wA3xvpE/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],"/[path]":["static/chunks/487-e26fa042908765fc.js","static/css/d260bf1012cb80c7.css","static/chunks/pages/[path]-0b502cf93cfd4f86.js"],sortedPages:["/_app","/_error","/[path]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
